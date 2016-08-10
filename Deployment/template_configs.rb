14d645df8db51a6dbdb5f1d1ac078fc60bf88644
random again <sspiezio@overdrive.com>
2016-08-10 16:00:33 -0400

    use result())

--- Deployment/template_configs.rb
+++ Deployment/template_configs.rb
@@ -1,6 +1,5 @@
 require('erb')
 
-serviceConfig = ERB.new(File.read('Deployment/ServiceConfiguration.Cloud.cscfg.erb')).result
+serviceConfig = ERB.new(File.read('Deployment/ServiceConfiguration.Cloud.cscfg.erb')).result()
 
 File.write('firstTry/ServiceConfiguration.Cloud.cscfg', serviceConfig)
-
