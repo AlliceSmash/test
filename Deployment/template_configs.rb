0fb7a9fa6043900d7df31a68b0e656f8d03f23eb
random again <sspiezio@overdrive.com>
2016-08-10 16:03:50 -0400

    modify rb file

--- Deployment/template_configs.rb
+++ Deployment/template_configs.rb
@@ -1,5 +1,6 @@
 require('erb')
-
-serviceConfig = ERB.new(File.read('Deployment/ServiceConfiguration.Cloud.cscfg.erb')).result()
+template = File.read('Deployment/ServiceConfiguration.Cloud.cscfg.erb')
+renderer = ERB.new(template)
+serviceConfig = renderer.result()
 
 File.write('firstTry/ServiceConfiguration.Cloud.cscfg', serviceConfig)
